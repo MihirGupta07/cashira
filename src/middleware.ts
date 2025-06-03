8bea3d8841bfdbe17c067992beb33cd52e1d44c6
MihirGupta07 <mihirgupta0712@gmail.com>
2025-06-03 14:24:10 +0530

    Update manifest.json to change icon paths to 'cashira-black-bg.png', remove favicon.ico, and adjust middleware to include 'assets' in the route exclusions.

--- src/middleware.ts
+++ src/middleware.ts
@@ -35,7 +35,8 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - public (public files)
+     * - assets (asset files)
      */
-    '/((?!_next/static|_next/image|favicon.ico|public|icons|manifest.json).*)',
+    '/((?!_next/static|_next/image|favicon.ico|public|icons|manifest.json|assets).*)',
   ],
 }; 
